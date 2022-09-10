e353e7bc1cb4209ee656bdc462ab5ab2471050d6
Vitaly Tochenyy <Antisvinn@yadnex.by>
2022-09-10 12:27:33 +0300

    delete navigate hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import styles from './app.module.css';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import GameField from './components/gameField/gameField';
-import { useEffect } from 'react';
 
 function App() {
-	const navigate = useNavigate();
-	useEffect(() => navigate('/main', { replace: true }), []);
-
 	return (
 		<div className={styles.app_container}>
 			<Routes>
-				<Route path='/main' element={<GameField />} />
+				<Route path='/' element={<GameField />} />
 			</Routes>
 		</div>
 	);
