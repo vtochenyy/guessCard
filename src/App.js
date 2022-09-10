f7725071e1e6107e710376489089504e80362a83
Vitaly Tochenyy <Antisvinn@yadnex.by>
2022-09-10 11:27:12 +0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import styles from './app.module.css';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import GameField from './components/gameField/gameField';
+import { useEffect } from 'react';
 
 function App() {
+	const navigate = useNavigate();
+	useEffect(() => navigate('/main'), []);
+
 	return (
 		<div className={styles.app_container}>
-			<GameField />
+			<Routes>
+				<Route path='/main' element={<GameField />} />
+			</Routes>
 		</div>
 	);
 }
