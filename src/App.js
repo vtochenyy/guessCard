cf8513eac82d07a095d370e2abb75b03e157e96d
Vitaly Tochenyy <Antisvinn@yadnex.by>
2022-09-10 12:23:28 +0300

    fix nab

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 function App() {
 	const navigate = useNavigate();
-	useEffect(() => navigate('/guessCard/main'), []);
+	useEffect(() => navigate('/main', { replace: true }), []);
 
 	return (
 		<div className={styles.app_container}>
