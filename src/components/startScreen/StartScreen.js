567e315b676a8b8d9565b1994e12a50bb57cfee7
Vitaly Tochenyy <Antisvinn@yadnex.by>
2022-09-10 14:54:29 +0300

    video

--- src/components/startScreen/StartScreen.js
+++ src/components/startScreen/StartScreen.js
@@ -3,21 +3,9 @@ import styles from './style.module.css';
 
 const StartScreen = () => {
 	return (
-		<>
-			<video
-				className={styles.videoIntro}
-				src='https://ru-wotp.wgcdn.co/static/5.110.0_53fd1f/wotp_static/img/core/frontend/scss/common/blocks/video-bg/img/video-bg.mp4'
-				preload
-				tabIndex='-1'
-				disablePictureInPicture
-				autoPlay
-				muted
-				loop
-			></video>
-			<Link className={styles.startGame} to='/guessCard/play'>
-				<span className={styles.startGameText}>Начать игру</span>
-			</Link>
-		</>
+		<Link className={styles.startGame} to='/guessCard/play'>
+			<span className={styles.startGameText}>Начать игру</span>
+		</Link>
 	);
 };
 
